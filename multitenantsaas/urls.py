ed500ba37c19190e41dd9e053b1af163760199db
Lanche <oreoluwaosinowo@yahoo.com>
2025-04-04 15:00:47 +0100

    Update urls.py

--- multitenantsaas/urls.py
+++ multitenantsaas/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 
 # GraphQL
 from apps.finances.schema import schema
